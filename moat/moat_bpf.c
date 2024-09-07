7b8ad61d90b71c92c75085e8015b070e5a498f66
AriSan <helloarisan@foxmail.com>
2024-09-07 18:26:16 +0800

    linking list for moat programs

--- moat/moat_bpf.c
+++ moat/moat_bpf.c
@@ -2,11 +2,44 @@
 #include <minos/print.h>
 #include <virt/vm.h>
 
+LIST_HEAD(moat_prog_list);
+
 unsigned long prints2(uint64_t base, int depth);
 
-unsigned long moat_bpf_mmap(void)
+uint64_t get_host_vttbr(void)
+{
+	void *pgdp;
+	uint64_t current_vttbr, host_vttbr;
+	struct vm *vm = get_host_vm();
+	pgdp = vm->mm.pgdp;
+	current_vttbr = read_sysreg(VTTBR_EL2);
+	host_vttbr = vtop(pgdp) | ((uint64_t)vm->vmid << 48);
+	pr_debug("current vttbr: %lx, host vttbr: %lx\n", current_vttbr, host_vttbr);
+	return host_vttbr;
+}
+
+struct moat_prog *get_moat_prog(uint32_t vmid)
+{
+	struct moat_prog *prog;
+	list_for_each_entry(prog, &moat_prog_list, list)
+	{
+		if (prog->vmid == vmid)
+			return prog;
+	}
+	return NULL;
+}
+unsigned long moat_bpf_mmap(uint32_t vmid)
 {
 	struct vm *vm = get_host_vm();
+	struct moat_prog *prog;
+
+	prog = get_moat_prog(vmid);
+	if (prog == NULL)
+	{
+		pr_err("no moat prog of vmid: %d\n", vmid);
+		return -ENOVMID;
+	}
+	
 	unsigned long ret = 114514;
 	unsigned long vttbr = 0;
 	unsigned long lvl_0_pa = 0;
@@ -25,16 +58,17 @@ unsigned long moat_bpf_mmap(void)
 	return ret;
 }
 
-void moat_bpf_unmmap(mm_struct *mm)
+void moat_bpf_unmmap(void)
 {
 
 }
 
+
 unsigned long moat_bpf_create(void)
 {
 	int ret = 0;
+	uint64_t vttbr = 0;
 	struct moat_prog *prog;
-	struct mm_struct *mm = &prog->mm;
 
 	prog = malloc(sizeof(struct moat_prog));
 	if (!prog)
@@ -49,25 +83,22 @@ unsigned long moat_bpf_create(void)
 	}
 
 	/* allocate stage-2 pgd for this BPF */
-	mm->pgdp = arch_alloc_guest_pgd();
-	if (mm->pgdp == NULL)
+	prog->pgdp = arch_alloc_guest_pgd();
+	if (prog->pgdp == NULL)
 	{
 		pr_err("No memory for vm page table\n");
 		return -ENOMEM;
 	}
 
-	unsigned long ret = 114514;
-	unsigned long vttbr = 0;
-	unsigned long lvl_0_pa = 0;
-	unsigned long content = 0;
-	asm volatile("mrs %0, vttbr_el2": "=r"(vttbr)::);
-	pr_notice("vttbr_el2: %lx\n", vttbr);
-	lvl_0_pa = ((((vttbr >> 1) >> 7) & 0xfffffffff) << 8);
-	asm volatile("ldr %0, [%1]": "=r"(content):"r"(lvl_0_pa):"memory");
-	pr_notice("content of vttbr_el2: %lx\n", content);
-	// ret = prints2(lvl_0_pa, 0);
+	init_list(&prog->list);
+	list_add_tail(&moat_prog_list, &prog->list);
+
+	vttbr = vtop(prog->pgdp) | ((uint64_t)prog->vmid << 48);
+
+	pr_info("allocated stage-2 pgd at: %lx, vttbr: %lx for vmid: %d\n", 
+			prog->pgdp, vttbr, prog->vmid);
 	
-	return ret;
+	return vttbr;
 }
 
 unsigned long prints2(uint64_t base, int depth)
